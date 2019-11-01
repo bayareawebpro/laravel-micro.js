149c453033a51da4bb4f6f5125997df42dabd427
Dan Alvidrez <dan@bayareawebpro.com>
2019-11-01 14:21:50 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import AppServiceProvider from './Services/App/AppServiceProvider'
 import ServiceProvider from './Support/ServiceProvider'
 import ErrorHandler from './Exceptions/ErrorHandler'
 import Exception from './Exceptions/Exception'
-import Collection from './Support/Collection'
 import Repository from './Support/Repository'
 import Validator from './Support/Validator'
 import Kernel from './Services/App/Kernel'
@@ -16,7 +15,6 @@ export {
     Pipeline,
     Exception,
     Container,
-    Collection,
     Validator,
     Repository,
     ErrorHandler,
